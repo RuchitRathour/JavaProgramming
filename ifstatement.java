866a59fc088281a37492a5fb74f4c3dfaf53a2e2
Ruchit Rathour <ruchitrathour@gmail.com>
2026-03-24 16:25:37 +0530

    Add files via upload

---
+++ ifstatement.java
@@ -0,0 +1,12 @@
+package controlstatements.DecisionMaking;
+
+public class ifstatement {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("You can vote");
+
+        }
+
+    }
+}
