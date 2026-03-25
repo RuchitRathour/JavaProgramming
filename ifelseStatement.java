eddac705e6dc6dbb2634fb5fa328b86f2d13d461
Ruchit Rathour <ruchitrathour@gmail.com>
2026-03-25 21:37:02 +0530

    Add files via upload

---
+++ ifelseStatement.java
@@ -0,0 +1,13 @@
+package controlstatements.DecisionMaking;
+
+public class ifelseStatement {
+    public static void main(String[] args) {
+        int number = 5;
+
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
