dbb28a41ad6794babba9c61b6a582096a9cf314e
Ruchit Rathour <ruchitrathour@gmail.com>
2026-03-28 16:23:23 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,9 @@
+package controlstatements.LoopingStatements;
+
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
