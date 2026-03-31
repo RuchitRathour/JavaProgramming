32d1b447d20fcc8a842c226f98d19adaa3b26eee
Ruchit Rathour <ruchitrathour@gmail.com>
2026-03-31 20:29:58 +0530

    Add files via upload

---
+++ breakStatement.java
@@ -0,0 +1,10 @@
+package ControlStatements.JumpStatements;
+
+public class breakStatement {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) break;
+            System.out.println(i);
+        }
+    }
+}
