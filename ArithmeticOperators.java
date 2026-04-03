b770395a45e117dab06b138343ad7899a4658288
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-03 18:53:43 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(++a); // 6, increments first
+        System.out.println(a++); // 6, prints first, then increments
+        System.out.println(a);   // 7
+    }
+}
