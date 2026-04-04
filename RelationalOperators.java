5b74cb9cfb91263f3861cd75f233de6da2e190e5
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-04 19:06:16 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,11 @@
+package Operators;
+
+public class RelationalOperators {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a && b); // false
+        System.out.println(a || b); // true
+        System.out.println(!a);     // false
+    }
+}
