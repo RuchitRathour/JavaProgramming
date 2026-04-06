2796af4a356008b8b1a27b079ed7f96d9a28df69
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-06 19:20:11 +0530

    Add files via upload

---
+++ BitwiseOperators.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        int a = 5;   // 101
+        int b = 3;   // 011
+        System.out.println(a & b);  // 001 = 1
+    }
+}
