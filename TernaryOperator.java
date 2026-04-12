7c349bbfab75dcda1795d5d5c69804b93b7692ab
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-12 17:14:47 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int max = (a > b) ? a : b; // if a>b then a else b
+        System.out.println(max); // 20
+    }
+}
