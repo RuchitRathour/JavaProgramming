6e78085c7297951c5523b880dfd757cde594bee5
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-14 23:34:37 +0530

    Add files via upload

---
+++ longDT.java
@@ -0,0 +1,8 @@
+package DataType.PrimitiveDataType;
+
+public class longDT {
+    public static void main(String[] args) {
+        long population = 15000000000L;
+        System.out.println(population);
+    }
+}
