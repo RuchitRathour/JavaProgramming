2e5a1eea716b9e943885e653a05e0c5de481fe35
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-15 10:03:08 +0530

    Add files via upload

---
+++ floatDT.java
@@ -0,0 +1,8 @@
+package DataType.PrimitiveDataType;
+
+public class floatDT {
+    public static void main(String[] args) {
+        float price = 99.5f;
+        System.out.println(price);
+    }
+}
