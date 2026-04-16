ab9ef02527cbf9c199659d8b9628a0f7dd22e53c
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-16 12:54:00 +0530

    Add files via upload

---
+++ booleanDT.java
@@ -0,0 +1,8 @@
+package DataType.PrimitiveDataType;
+
+public class booleanDT {
+    public static void main(String[] args) {
+        boolean isPassed = true;
+        System.out.println(isPassed);
+    }
+}
