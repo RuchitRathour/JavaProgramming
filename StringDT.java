d2c5fd7d92b57ce22fdcbf117e2fd47965a88934
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-17 11:18:31 +0530

    Add files via upload

---
+++ StringDT.java
@@ -0,0 +1,8 @@
+package DataType.PrimitiveDataType;
+
+public class StringDT {
+    public static void main(String[] args) {
+        String name = "Ruchit";
+        System.out.println(name);
+    }
+}
