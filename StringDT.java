89d30357903b0b58339292253dfff9d02e2f4804
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-18 21:40:49 +0530

    Update StringDT.java

--- StringDT.java
+++ StringDT.java
@@ -4,6 +4,7 @@ public class StringDT {
     public static void main(String[] args) {
         String name = "Ruchit";
         String name2 = "Kumar";
+        String name3 = "Rohit";
         System.out.println(name);
         System.out.println(name2);
     }
