d87eb73e941c81de0f355578d51ede6aaa35c89e
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-18 21:39:26 +0530

    Update StringDT.java

--- StringDT.java
+++ StringDT.java
@@ -3,6 +3,8 @@ package DataType.PrimitiveDataType;
 public class StringDT {
     public static void main(String[] args) {
         String name = "Ruchit";
+        String name2 = "Kumar";
         System.out.println(name);
+        System.out.println(name2);
     }
 }
