b25465ab1f0fc0896d841e12503a2618a7970247
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-19 21:34:28 +0530

    Update charDT.java

--- charDT.java
+++ charDT.java
@@ -3,6 +3,7 @@ package DataType.PrimitiveDataType;
 public class charDT {
     public static void main(String[] args) {
         char grade = 'A';
+        char mathgrade = 'B';
         System.out.println(grade);
     }
 }
