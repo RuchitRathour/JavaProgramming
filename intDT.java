d4cc778503f58308d9337a0a186a423c866936d9
Ruchit Rathour <ruchitrathour@gmail.com>
2026-04-20 22:32:39 +0530

    Update intDT.java

--- intDT.java
+++ intDT.java
@@ -3,6 +3,7 @@ package DataType.PrimitiveDataType;
 public class intDT {
     public static void main(String[] args) {
         int age = 20;
+        int rollno = 70;
         System.out.println(age);
     }
 }
